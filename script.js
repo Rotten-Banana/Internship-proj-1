c01bb21226697b7c3c435906c1ea3dd5c1826f26
Souvik Srimani <55970375+Rotten-Banana@users.noreply.github.com>
2021-01-03 22:29:46 +0530

    responsive and some js update

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 $(document).ready(()=>{
-	$("button.blue").click(()=>{
+	$("button#refresh").click(()=>{
 		console.log("click");
 	})
 
 
 	$("#network").click(()=>{
+		$("#unready_content").hide();
 		$("#Network").show();
-		$("#Devices").hide();
 	})
 
 	$("#device").click(()=>{
-		$("#Devices").show();
 		$("#Network").hide();
+		$("#unready_content").show();
+	})
+
+	$("#others").click(()=>{
+		$(".side_nav ul").not("#other_nav").hide();
+		$("#other_nav").show();
+		$("#device").trigger("click");
+	})
+
+	$("#setup").click(()=>{
+		$(".side_nav ul").not("#setup_nav").hide();
+		$("#setup_nav").show();
+		$("#network").trigger("click");
+	})
+
+	$("#some").click(()=>{
+		$(".side_nav ul").not("#some_nav").hide();
+		$("#some_nav").show();
+		$("#device").trigger("click");
 	})
 })
